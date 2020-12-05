b7e477f04bcfd7fa72f51ad476a1b86865372c3e
alorti02 <59167063+alorti02@users.noreply.github.com>
2020-12-05 22:40:05 +0100

    Update Ejercicio6.cpp

--- Practica_2.1/Ejercicio6.cpp
+++ Practica_2.1/Ejercicio6.cpp
@@ -5,9 +5,9 @@
 #include <sys/utsname.h>
 
 int main(){
-	printf("Longitud maxima de argumentos: %i\n",sysconf(_SC_ARG_MAX));
-	printf("Numero maximo de hijos: %i\n",sysconf(_SC_CHILD_MAX));
-	printf("Numero maximo de ficheros: %i\n",sysconf(_SC_OPEN_MAX));
+	printf("Longitud maxima de argumentos: %i \n", sysconf(_SC_ARG_MAX));
+	printf("Numero maximo de hijos: %i \n", sysconf(_SC_CHILD_MAX));
+	printf("Numero maximo de ficheros: %i \n", sysconf(_SC_OPEN_MAX));
 
   return 0;
 }
