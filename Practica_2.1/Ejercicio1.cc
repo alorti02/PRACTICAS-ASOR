02a54ad7fb638b4da6a2b1064294127ded93c5fc
alorti02 <59167063+alorti02@users.noreply.github.com>
2020-12-05 21:43:02 +0100

    Update Ejercicio1.cc
    Funciona OK

--- Practica_2.1/Ejercicio1.cc
+++ Practica_2.1/Ejercicio1.cc
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 
+int main(){
+    char *s;
+
+    if(setuid(2) == -1){
+        perror(s);
+    }
+    
+    else{
+        printf("Todo OK.");
+    }
+
+    return 1;
+}
