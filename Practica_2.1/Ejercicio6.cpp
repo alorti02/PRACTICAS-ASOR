dfc8bd198b8dcb9370f443cc20e7d01683ff4be8
alorti02 <59167063+alorti02@users.noreply.github.com>
2020-12-05 22:32:25 +0100

    Create Ejercicio6.cpp

---
+++ Practica_2.1/Ejercicio6.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/utsname.h>
+
+int main(){
+	printf("Longitud maxima de argumentos: %i\n",sysconf(_SC_ARG_MAX));
+	printf("Numero maximo de hijos: %i\n",sysconf(_SC_CHILD_MAX));
+	printf("Numero maximo de ficheros: %i\n",sysconf(_SC_OPEN_MAX));
+
+  return 0;
+}
